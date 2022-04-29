6f086bc172d97df980054c98522138d8f6349d39
Jonas Leuchtenberger <jonas.leuchtenberger@rwth-aachen.de>
2022-04-29 10:59:32 +0200

    Added some TODO's in dataset_example file

--- dataset_example_usage.py
+++ dataset_example_usage.py
@@ -1,5 +1,27 @@
 from uah_dataset.pandas_importer import UAHDataset
 
+# TODO: create entry for each time step: problem is "how to fill missing values?"
+# approach 1: simply repeat previous values
+# approach 2: calculate some kind of regression or use splines
+# approach X: ?
+# EFFORT: 6h? (depending on approach and existing data)
+# Florene
+
+# TODO: find missing headers: have a look into their data reader again
+# EFFORT: 2h (at most)
+# Jonas
+
+# TODO: sample frame of video corresponding to sensor data depending on time step
+# EFFORT: 2h (depending on libraries i can use)
+# Jonas or Florene
+
+# TODO: apply some preprocessing on image data (image normalization, contrast sharpening, …)
+# EFFORT depends on the amount of preprocessing
+# Jonas
+
+# TODO: save this form of dataset created by us and save it (using pandas and/or our own datastructure)
+# Florene or Jonas
+
 # create the dataset
 dataset = UAHDataset()
 print("Dataset:", dataset.latest)
