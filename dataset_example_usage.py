2dcc864d24917b467bce973935bf26e175280e6b
FlipFlop13 <93262834+FlipFlop13@users.noreply.github.com>
2022-06-06 13:38:39 +0200

    06/06/2022

--- dataset_example_usage.py
+++ dataset_example_usage.py
@@ -2,7 +2,10 @@ import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
 import tensorflow as tf
-
+import copy
+import matplotlib.pyplot as plt
+import os
+import imageio
 
 from uah_dataset.pandas_importer import UAHDataset
 
@@ -22,52 +25,53 @@ from uah_dataset.pandas_importer import UAHDataset
 # UAHDataset(generate_video_frames=True)
 
 # create the dataset
-dataset = UAHDataset()
-print("Dataset:", dataset.latest)
-print("Drivers:", dataset.drivers)
+# dataset = UAHDataset()
+# print("Dataset:", dataset.latest)
+# print("Drivers:", dataset.drivers)
 
 
-#test
+# #test
 
 
 
-'''# read one specific driver
-print("Info of driver D1")
-road_type_dict = dataset.dataframe_by_driver("D1", skip_missing_headers=True, suppress_warings=True)
-roads = [_ for _ in road_type_dict.keys()]
-print("Roads:\t", roads)
-print("# Roads:", sum([len(road_type_dict[_]) for _ in roads]))
+# '''# read one specific driver
+# print("Info of driver D1")
+# road_type_dict = dataset.dataframe_by_driver("D1", skip_missing_headers=True, suppress_warings=True)
+# roads = [_ for _ in road_type_dict.keys()]
+# print("Roads:\t", roads)
+# print("# Roads:", sum([len(road_type_dict[_]) for _ in roads]))
 
-# read all drivers
-# note: this should work, but we loose information about when a time series resets. 
-# This could prevent learning later on.
-print("Info of all drivers")'''
-road_type_dict = dataset.dataframe(skip_missing_headers=True, suppress_warings=True)
-'''roads = [_ for _ in road_type_dict.keys()]
-print("# Roads:", sum([len(road_type_dict[_]) for _ in roads]))
+# # read all drivers
+# # note: this should work, but we loose information about when a time series resets.
+# # This could prevent learning later on.
+# print("Info of all drivers")'''
+# road_type_dict = dataset.dataframe(skip_missing_headers=True, suppress_warings=True)
 
-for r, rd in road_type_dict.items():
-    print(f"\t{r} - {len(rd)}")
-    for l, ld in rd.items():
-        print(f"\t\t{l} - {len(ld)}")
+# '''roads = [_ for _ in road_type_dict.keys()]
+# print("# Roads:", sum([len(road_type_dict[_]) for _ in roads]))
 
-# process one specific dataframe'''
-array = []
-for road_type in road_type_dict.items():
-    print(1)
+# for r, rd in road_type_dict.items():
+#     print(f"\t{r} - {len(rd)}")
+#     for l, ld in rd.items():
+#         print(f"\t\t{l} - {len(ld)}")
 
-    skip = True
-    for route in road_type[1].items():
-        mean = np.array(route[1].mean())
-        array.append(mean)
+# # process one specific dataframe'''
+# array = []
+# for road_type in road_type_dict.items():
+#     print(1)
 
-#print(road_type_dict["SECONDARY"]["NORMAL"][['Altitude', 'Longitude', 'Latitude']].head(5))
-#'Altitude', 'Longitude', 'Latitude'
+#     skip = True
+#     for route in road_type[1].items():
+#         mean = np.array(route[1].mean())
+#         array.append(mean)
 
-fused_data = np.array(array)
-fused_data[np.isnan(fused_data)]=0
+# #print(road_type_dict["SECONDARY"]["NORMAL"][['Altitude', 'Longitude', 'Latitude']].head(5))
+# #'Altitude', 'Longitude', 'Latitude'
 
-kmeans = KMeans(n_clusters=2, random_state=0).fit(fused_data)
+# fused_data = np.array(array)
+# fused_data[np.isnan(fused_data)]=0
+
+# kmeans = KMeans(n_clusters=2, random_state=0).fit(fused_data)
 
 
 
@@ -110,4 +114,110 @@ history = model.fit(X_train, X_train, epochs=200, batch_size=16, verbose=2, vali
 
 
 '''We will finaly cluster based on the representation'''
-print(kmeans.labels_)
+# print(kmeans.labels_)
+
+
+
+
+
+
+
+
+def windowing(dictionary : dict ,rows_per_minute : int = 360, initial_threshold : int = 60, increment : int = 10) -> dict:
+    """
+    Creates windows, for every
+    :param dictionary: nested dic road types -> mood -> dataframe
+    :param rows_per_minute: number of rows that on averag consistute one minute
+    :param initial_threshold: timestamp when we start to approve the windows
+    :param increment: timestamp difference between end of adjacent windows
+    :return: dictionary: road type -> mood -> window_index -> dataframe
+    """
+
+    window_number = 0
+    window_time = 0
+    time_difference = []
+    for road, road_dic in dict.items():
+        i = 0
+        print(f"______________________Road: {road} _________________________________")
+        for mood, mood_df in road_dic.items():
+            windowed = {}
+            t = initial_threshold                                    #first window ends at a point where more than t seconds have passed
+            print(f"Mood: {mood}")
+            for window in mood_df.rolling(window = rows_per_minute):
+                if window.iloc[-1, 0] < window.iloc[0, 0]:  # meaning we have finished one driver trip, as the nnext df values are lower than the previous
+                    t = initial_threshold
+                elif int(window.iloc[-1, 0]) > t:
+                    windowed[i] = window
+                    i += 1
+                    t += increment                     #creates 10 second windows
+                    window_number += 1
+                    time = (window.iloc[-1, 0]-window.iloc[0,0])+1
+                    window_time += time
+                    time_difference.append(time)
+
+            windowed_dic[road][mood] = windowed
+
+    print(f"Average window timelapse: {window_time/window_number}")
+    print(f"Number of windows: {window_number}")
+    plt.hist(time_difference, bins = 120)
+    plt.title(f"Window timelapse for {rows_per_minute} data points.")
+    plt.xlabel("Seconds")
+    plt.show()
+    return windowed_dic
+
+
+
+
+
+
+def read(path_to_uah_folder: str = f"{os.path.dirname(__file__)}/uah_dataset/UAH-DRIVESET-v1/"):
+    """
+    Mainly copied from the original reader
+    :param path_to_uah_folder:
+    :return: Online semantics, in nested dictionary ->road type -> mood -> dataframe for all driver on this mood and road type
+    """
+    root_dir = "./uah_dataset/"
+    latest = "UAH-DRIVESET-v1/"
+    drivers  = ['D1','D2','D3','D4','D5','D6']
+    roads = ["MOTORWAY", "SECONDARY"]
+
+    headers = ["TimeStamp", "Latitude" , " Longitude", "Total" , "Accel", "Braking", "Turning", "Weaving",
+                "Drifting", "Oversspeed", "Carfollow", "Normal", "Drowsy", "Aggressive", "Unknown",
+                "Total_last_minute", "Accel_last_minute", "Braking_last_minute" , "Turning_last_minute",
+                "Weaving_last_minute","Drifting_last_minute", "Oversspeed_last_minute", "Carfollow_last_minute",
+                "Normal_last_minute", "Drowsy_last_minute", "Aggressive_last_minute", "Unknown_last_minute"
+                ]
+    online_semantics = {}
+    for driver in drivers:
+        folder = f"{path_to_uah_folder}{driver}"
+        for direc in os.listdir(folder):
+            splitted_string = direc.split('-')
+            road = splitted_string[-1]
+            mood = splitted_string[-2]
+            scoresFileName = folder + '/' + direc + '/' + 'SEMANTIC_ONLINE.txt'
+            scoresData = np.genfromtxt(scoresFileName, dtype=np.float64, delimiter=' ')
+            df = pd.DataFrame(scoresData, columns = headers)
+
+            if road in online_semantics:
+                if mood in online_semantics[road]:
+                    online_semantics[road][mood] = pd.concat([online_semantics[road][mood], df])
+                else:
+                    online_semantics[road][mood] = df
+            else:
+                online_semantics.update({road : {mood : df}})
+
+
+    return online_semantics
+
+
+
+
+
+if __name__ == "__main__":
+    # windowed_dic = copy.deepcopy(road_type_dict)
+    # rows_per_minute = 400  # for dataframe, doesnt work consistently
+    # windowing(windowed_dic, rows_per_minute=rows_per_minute)
+    windowed_dic = read()
+    rows_per_minute = 60 #for online semantics
+    windowing(windowed_dic, rows_per_minute=rows_per_minute)
+
