d6dd9c3f7e4c8cfbfe6b2a157a19c37e969f52cb
Nikolaos Ntantis <nickd_96@hotmail.com>
2022-06-08 21:31:37 +0200

    memory efficient numpy

--- uah_dataset/dataset.py
+++ uah_dataset/dataset.py
@@ -1,19 +1,39 @@
 import torch
+import numpy as np
 
 
 class Dataset(torch.utils.data.Dataset):
     def __init__(self) -> None:
         super().__init__()
 
-        # load all matrices
+        # get shapes of files
+        """with open('../train.npy', 'rb') as f:
+            #major, minor = np.lib.format.read_magic(f)
+            sensor_shape, _, sensor_dtype = np.lib.format.read_array_header_1_0(f)
+        with open('../labels.npy', 'rb') as f:
+            #major, minor = np.lib.format.read_magic(f)
+            image_shape, _, image_dtype = np.lib.format.read_array_header_1_0(f)
+            
+        #load all matrices
+        self.sensor_data = np.memmap('../train.npy', dtype=sensor_dtype, mode='w+', shape=sensor_shape)
+        self.image_data = np.random.rand(2937, 400, 2, 2, 1)
+        self.labels = np.memmap('../labels.npy', dtype=image_dtype, mode='w+', shape=image_shape)"""
 
+        # load all matrices
+        self.sensor_data = np.memmap('../train_processed.dat', dtype='float32', mode='r', shape=(2937, 400, 36))
+        self.image_data = np.random.rand(2937, 400, 2, 2, 1)
+        self.labels = np.memmap('../labels_processed.dat', dtype='int', mode='r', shape=(2937, 3))
     def __len__(self) -> int:
         # amount of total samples / windows / whatever we train
-        return 0
+        if (len(self.sensor_data) != len(self.image_data)):
+            raise RuntimeError("Sensor data and video data have different sizes")
+            return -1
+        return len(self.labels)
 
     def __getitem__(self, index):
-        #
-        return None
+        # TODO maybe do a check before returning
+        #print(self.labels)
+        return ((self.sensor_data[index], self.image_data[index]), self.labels[index])
 
 
 if __name__ == "__main__":
@@ -21,4 +41,9 @@ if __name__ == "__main__":
     d = Dataset()
 
     print(len(d))
-    (sensor_data, image_data), label = d[0]
+    (sensor_data, image_data), label = d[...]
+    print(np.max(sensor_data))
+    print(image_data)
+    print(label)
+    #print(label)
+
