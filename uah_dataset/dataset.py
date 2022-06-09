a8957068ad0d96873dc82b214d00480183bb73d5
Jonas Leuchtenberger <jonas.leuchtenberger@rwth-aachen.de>
2022-06-09 11:56:46 +0200

    fixed missing parameter fixed wrong *.dat path

--- uah_dataset/dataset.py
+++ uah_dataset/dataset.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 class Dataset(torch.utils.data.Dataset):
-    def __init__(self) -> None:
+    def __init__(self, d_type: str = "train") -> None:
         super().__init__()
 
         # get shapes of files
@@ -20,14 +20,24 @@ class Dataset(torch.utils.data.Dataset):
         self.labels = np.memmap('../labels.npy', dtype=image_dtype, mode='w+', shape=image_shape)"""
 
         # load all matrices
-        self.sensor_data = np.memmap('../train_processed.dat', dtype='float32', mode='r', shape=(2937, 400, 36))
+        self.sensor_data = np.memmap('./train_processed.dat', dtype='float32', mode='r', shape=(2937, 400, 36))
         self.image_data = np.random.rand(2937, 400, 2, 2, 1)
-        self.labels = np.memmap('../labels_processed.dat', dtype='int', mode='r', shape=(2937, 3))
+        self.labels = np.memmap('./labels_processed.dat', dtype='int', mode='r', shape=(2937, 3))
+
+        if d_type == "train":
+            # TODO: training data
+            pass
+        elif d_type == "test":
+            # TODO: testing data
+            pass
+        else:
+            raise RuntimeError("Unsupported dataset type: " + d_type)
+
     def __len__(self) -> int:
         # amount of total samples / windows / whatever we train
         if (len(self.sensor_data) != len(self.image_data)):
             raise RuntimeError("Sensor data and video data have different sizes")
-            return -1
+
         return len(self.labels)
 
     def __getitem__(self, index):
@@ -37,7 +47,7 @@ class Dataset(torch.utils.data.Dataset):
 
 
 if __name__ == "__main__":
-    # TODO: perform our tests
+    # perform some tests
     d = Dataset()
 
     print(len(d))
