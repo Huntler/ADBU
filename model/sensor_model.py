b1972bfbe516af31d2663292d7c16b3821fe6be0
Jonas Leuchtenberger <jonas.leuchtenberger@rwth-aachen.de>
2022-06-10 19:21:31 +0200

    Fixed sensor model

--- model/sensor_model.py
+++ model/sensor_model.py
@@ -8,28 +8,26 @@ class SensorModel(nn.Module):
         super(SensorModel, self).__init__()
 
         # 1D convolutional to detect patterns within one feature
-        self.__conv_1 = nn.Conv1d(400, 400, 10, stride=2)
-        self.__bn_1 = nn.BatchNorm1d(200)
+        self.__conv_1 = nn.Conv1d(400, 400, 8, 1)
+        self.__bn_1 = nn.BatchNorm1d(400)
 
-        self.__conv_2 = nn.Conv1d(200, 200, 10, stride=2)
-        self.__bn_1 = nn.BatchNorm1d(100)
+        self.__conv_2 = nn.Conv1d(400, 400, 4, 1)
+        self.__bn_2 = nn.BatchNorm1d(400)
 
-        self.__conv_3 = nn.Conv1d(100, 100, 10, stride=2)        
-        self.__bn_1 = nn.BatchNorm1d(100)
+        self.__conv_3 = nn.Conv1d(400, 400, 4, 1) 
+        self.__bn_3 = nn.BatchNorm1d(400)       
     
     def forward(self, x) -> Any:
         x = self.__conv_1(x)
-        x = nn.MaxPool1d(2)
         x = self.__bn_1(x)
         x = torch.tanh(x)
         
         x = self.__conv_2(x)
-        x = nn.MaxPool1d(2)
         x = self.__bn_2(x)
         x = torch.tanh(x)
 
         x = self.__conv_3(x)
-        x = self.__bn_2(x)
+        x = self.__bn_3(x)
         x = torch.tanh(x)
 
         return x
